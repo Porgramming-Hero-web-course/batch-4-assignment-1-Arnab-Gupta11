d30a06998aba4c6123f25dcbd29fadb8032c213c
Arnab Gupta <120601811+Arnab-Gupta11@users.noreply.github.com>
2024-11-08 22:17:36 +0600

    Update problem-7.ts

--- problem-7.ts
+++ problem-7.ts
@@ -2,10 +2,9 @@
   //Problem-7
   class Car {
     constructor(public make: string, public model: string, public year: number) {}
-    getCarAge(): string {
+    getCarAge(): number {
       const currentYear = new Date().getFullYear();
-      let age = currentYear - this.year;
-      return `${age} (assuming current year is ${currentYear})`;
+      return currentYear - this.year;
     }
   }
 }
